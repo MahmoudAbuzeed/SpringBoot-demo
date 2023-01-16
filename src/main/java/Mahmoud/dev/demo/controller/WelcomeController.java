8c03a0a719648c3407186beaa412c9c91d647171
mahmoudabuzeed3 <mahmoud.abuzeed@jumia.com>
2023-01-16 03:04:41 +0200

    inject welcome service into welcome controller

--- src/main/java/Mahmoud/dev/demo/controller/WelcomeController.java
+++ src/main/java/Mahmoud/dev/demo/controller/WelcomeController.java
@@ -1,11 +1,18 @@
 package Mahmoud.dev.demo.controller;
 
+import Mahmoud.dev.demo.service.WelcomeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    private WelcomeService welcomeService;
+
+    public WelcomeController(WelcomeService welcomeService) {
+        this.welcomeService = welcomeService;
+    }
 
     @GetMapping("/welcome")
     public String welcome(@RequestParam(defaultValue = "Stranger") String name){
