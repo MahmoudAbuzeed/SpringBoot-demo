2a3de414344dcdc292f1f03efe23daccd5e6699d
mahmoudabuzeed3 <mahmoud.abuzeed@jumia.com>
2023-01-17 20:27:58 +0200

    fix defualt constructor issue

--- src/main/java/Mahmoud/dev/demo/model/Product.java
+++ src/main/java/Mahmoud/dev/demo/model/Product.java
@@ -9,6 +9,13 @@ import java.util.Objects;
 
 @Entity
 public class Product {
+
+    public Product(int id, String name, Double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
@@ -39,12 +46,7 @@ public class Product {
         this.quantity = quantity;
     }
 
-    public Product(int id, String name, Double price, int quantity) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
+
 
     @Override
     public String toString() {
